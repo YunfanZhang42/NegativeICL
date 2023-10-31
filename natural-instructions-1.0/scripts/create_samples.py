09da50752c9bc3fe78b6f112415459ded646e221
marcusm117 <marcusmin117@gmail.com>
2023-10-31 14:46:32 -0400

    update create_samples.py

--- natural-instructions-1.0/scripts/create_samples.py
+++ natural-instructions-1.0/scripts/create_samples.py
@@ -2,58 +2,78 @@ import argparse
 import json
 
 
+# we try our best to follow the context template in the paper
+# see https://arxiv.org/abs/2104.08773, Appendix B
 def get_context(data):
     context = f"""Title: {data['Title']}
 
 Definition: {data['Definition']}
 
-Emphasis & Caution: {data['Emphasis & Caution']}
+Prompt: {data['Prompt']}
 
 Things to Avoid: {data['Things to Avoid']}
 
-Positive Examples:
+Emphasis & Caution: {data['Emphasis & Caution']}
 
 """
 
+    context += "Negative Examples:"
+    # check if negative examples are present
+    if "Negative Examples" not in data['Examples']:
+        context += " -\n"
+    else:
+        context += "\n"
+        for i in range(len(data['Examples']['Negative Examples'])):
+            # check if reason is present in the example
+            if "reason" not in data['Examples']['Negative Examples'][i]:
+                # following in the format in the paper
+                # see https://arxiv.org/abs/2104.08773
+                reason = "-"
+            else:
+                reason = data['Examples']['Negative Examples'][i]['reason']
+            # check if suggestion is present in the example
+            if "suggestion" not in data['Examples']['Negative Examples'][i]:
+                # following in the format in the paper
+                # see https://arxiv.org/abs/2104.08773
+                suggestion = "-"
+            else:
+                suggestion = data['Examples']['Negative Examples'][i]['suggestion']
+            context += f"""Negative Example {i+1} -
+    Input: {data['Examples']['Negative Examples'][i]['input']}
+    Output: {data['Examples']['Negative Examples'][i]['output']}
+    Reason: {reason}
+    Suggestion: {suggestion}
+
+"""
+
+    context += """Positive Examples:
+"""
+
     for i in range(len(data['Examples']['Positive Examples'])):
-        # checking if suggestions is present in the example
+        # check if reason is present in the example
+        if "reason" not in data['Examples']['Positive Examples'][i]:
+            # following in the format in the paper
+            # see https://arxiv.org/abs/2104.08773
+            reason = "-"
+        else:
+            reason = data['Examples']['Positive Examples'][i]['reason']
+        # check if suggestion is present in the example
         if "suggestion" not in data['Examples']['Positive Examples'][i]:
             # following in the format in the paper
             # see https://arxiv.org/abs/2104.08773
             suggestion = "-"
         else:
             suggestion = data['Examples']['Positive Examples'][i]['suggestion']
-        context += f"""Input: {data['Examples']['Positive Examples'][i]['input']}
-Output: {data['Examples']['Positive Examples'][i]['output']}
-Reason: {data['Examples']['Positive Examples'][i]['reason']}
-Suggestion: {suggestion}
-
-"""
-
-    context += """Negative Examples:
-"""
-
-    for i in range(len(data['Examples']['Negative Examples'])):
-        # checking if suggestions is present in the example
-        if "suggestion" not in data['Examples']['Negative Examples'][i]:
-            # following in the format in the paper
-            # see https://arxiv.org/abs/2104.08773
-            suggestion = "-"
-        else:
-            suggestion = data['Examples']['Negative Examples'][i]['suggestion']
-        context += f"""Input: {data['Examples']['Negative Examples'][i]['input']}
-Output: {data['Examples']['Negative Examples'][i]['output']}
-Reason: {data['Examples']['Negative Examples'][i]['reason']}
-Suggestion: {suggestion}
-
-"""
-
-    context += f"""Prompt: {data['Prompt']}
+        context += f"""Positive Example {i+1} -
+    Input: {data['Examples']['Positive Examples'][i]['input']}
+    Output: {data['Examples']['Positive Examples'][i]['output']}
+    Reason: {reason}
+    Suggestion: {suggestion}
 
 """
 
     return context
-    
+
 
 def main():
     parser = argparse.ArgumentParser()
