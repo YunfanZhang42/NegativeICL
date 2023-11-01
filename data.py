131996985a0dc2579671ff720535cfc29dd51af2
Yunfan Zhang <z4y5f3@gmail.com>
2023-10-31 23:09:57 -0400

    Updated training and data loader code to support Natural Instructions v1 and Seq2Seq LMs.

--- data.py
+++ data.py
@@ -1,27 +1,200 @@
+import argparse
+import time
+import json
+import os
+import random
+from dotmap import DotMap
 from torch.utils.data import Dataset
-from datasets import load_dataset
+from transformers import T5TokenizerFast, BartTokenizerFast
 
 
-class HFCLMDataset(Dataset):
-    def __init__(self, hf_dataset_path, hf_dataset_name, tokenizer, dataset_type="train", max_length=1024):
-        def tokenize_function(examples):
-            return tokenizer(examples["text"], padding="max_length", truncation=True, max_length=max_length, return_tensors="pt")
+HF_LOSS_IGNORE_TOKEN_ID = -100
 
-        dataset = load_dataset(hf_dataset_path, hf_dataset_name)
-        tokenizer.pad_token = tokenizer.eos_token
-        tokenizer.padding_side = "right"
 
-        tokenized_datasets = dataset.map(tokenize_function, batched=True)
-        tokenized_datasets.set_format(type="torch", columns=["input_ids", "attention_mask"])
+class NaturalInstructionsV1Seq2SeqDataset(Dataset):
+    def __init__(
+        self,
+        subtask_dir="./natural-instructions-1.0/data/",
+        tasks=[],
+        template="./natural_instructions_v1_template.txt",
+        pos_template="./natural_instructions_v1_positive_examples_template.txt",
+        neg_template="./natural_instructions_v1_negative_examples_template.txt",
+        tokenizer=None,
+        padding="max_length",
+        max_input_length=1024,
+        max_output_length=128,
+    ):
+        super().__init__()
 
-        self.dataset = tokenized_datasets[dataset_type]
+        self.sample_inputs = []
+        self.sample_outputs = []
+        self.tokenizer = tokenizer
+        self.tokenizer.padding_side = "right"
+        self.padding = padding
+        self.max_input_length = max_input_length
+        self.tokenizer.model_max_length = max_input_length
+        self.max_output_length = max_output_length
+
+        for task in tasks:
+            task_path = os.path.join(subtask_dir, task)
+            with open(task_path, "r") as f:
+                task_data = json.load(f)
+            self.add_task_to_samples(task_data, template, pos_template, neg_template)
+
+    def format_examples(self, template, example_data):
+        # Shuffle the examples, format them to strings, while also calculating their lengths.
+        random.shuffle(example_data)
+        examples_str_and_lengths = []
+        for i, example in enumerate(example_data):
+            reason = example["reason"] if "reason" in example else "N/A"
+            suggestion = example["suggestion"] if "suggestion" in example else "N/A"
+            example_str = template.format(
+                example_index=str(i + 1), input=example["input"], output=example["output"], reason=reason, suggestion=suggestion
+            )
+            example_length = len(self.tokenizer(example_str, padding=False)["input_ids"])
+            examples_str_and_lengths.append((example_str, example_length))
+        return examples_str_and_lengths
+
+    def add_task_to_samples(self, task_data, template, pos_template, neg_template):
+        # Read the templates
+        with open(template, "r") as f:
+            template = f.read().strip()
+        with open(pos_template, "r") as f:
+            pos_template = f.read().strip()
+        with open(neg_template, "r") as f:
+            neg_template = f.read().strip()
+
+        for sample in task_data["Instances"]:
+            # First, calculate the length of the input without any examples
+            input_without_examples = template.format(
+                title=task_data["Title"],
+                definition=task_data["Definition"],
+                prompt=task_data["Prompt"],
+                avoid=task_data["Things to Avoid"],
+                emphasis=task_data["Emphasis & Caution"],
+                negative_examples="",
+                positive_examples="",
+                input=sample["input"],
+            )
+
+            input_without_examples_length = len(self.tokenizer(input_without_examples, padding=False)["input_ids"])
+
+            # Next, shuffle and format the examples
+            positive_examples = []
+            if (
+                "Positive Examples" in task_data["Examples"]
+                and task_data["Examples"]["Positive Examples"]
+                and task_data["Examples"]["Positive Examples"][0] != "-"
+            ):
+                positive_examples = self.format_examples(pos_template, task_data["Examples"]["Positive Examples"])
+                positive_examples.reverse()
+
+            negative_examples = []
+            if (
+                "Negative Examples" in task_data["Examples"]
+                and task_data["Examples"]["Negative Examples"]
+                and task_data["Examples"]["Negative Examples"][0] != "-"
+            ):
+                negative_examples = self.format_examples(neg_template, task_data["Examples"]["Negative Examples"])
+                negative_examples.reverse()
+
+            # While the length is not exceeded, add alternating positive and negative examples
+            selected_positive_examples = []
+            selected_negative_examples = []
+            current_input_length = input_without_examples_length
+
+            while len(positive_examples) > 0 or len(negative_examples) > 0:
+                if len(positive_examples) > 0:
+                    next_positive_example = positive_examples.pop()
+                    next_positive_example_str = next_positive_example[0]
+                    next_positive_example_length = next_positive_example[1]
+                    if current_input_length + next_positive_example_length > self.max_input_length:
+                        break
+                    else:
+                        current_input_length += next_positive_example_length
+                        selected_positive_examples.append(next_positive_example_str)
+                if len(negative_examples) > 0:
+                    next_negative_example = negative_examples.pop()
+                    next_negative_example_str = next_negative_example[0]
+                    next_negative_example_length = next_negative_example[1]
+                    if current_input_length + next_negative_example_length > self.max_input_length:
+                        break
+                    else:
+                        current_input_length += next_negative_example_length
+                        selected_negative_examples.append(next_negative_example_str)
+
+            # Format the final input with the selected examples.
+            positive_examples_str = "\n".join(selected_positive_examples)
+            negative_examples_str = "\n".join(selected_negative_examples)
+
+            input_with_examples = template.format(
+                title=task_data["Title"],
+                definition=task_data["Definition"],
+                prompt=task_data["Prompt"],
+                avoid=task_data["Things to Avoid"],
+                emphasis=task_data["Emphasis & Caution"],
+                negative_examples=negative_examples_str.strip(),
+                positive_examples=positive_examples_str.strip(),
+                input=sample["input"],
+            )
+
+            # Randomly select a reference output.
+            output = random.choice(sample["output"])
+
+            self.sample_inputs.append(input_with_examples)
+            self.sample_outputs.append(output)
 
     def __len__(self):
-        return len(self.dataset)
+        return len(self.sample_inputs)
 
     def __getitem__(self, idx):
+        inputs = self.tokenizer(self.sample_inputs[idx], padding=self.padding, max_length=self.max_input_length, truncation=True, return_tensors="pt")
+        outputs = self.tokenizer(self.sample_outputs[idx], padding=self.padding, max_length=self.max_output_length, truncation=True, return_tensors="pt")
+        labels = outputs["input_ids"]
+        labels[labels == self.tokenizer.pad_token_id] = HF_LOSS_IGNORE_TOKEN_ID
+
         return {
-            "input_ids": self.dataset[idx]["input_ids"],
-            "attention_mask": self.dataset[idx]["attention_mask"],
-            "labels": self.dataset[idx]["input_ids"]
+            "input_ids": inputs["input_ids"].flatten(),
+            "attention_mask": inputs["attention_mask"].flatten(),
+            # Hugging Face's Seq2Seq models needs a labels argument, and should be able to generate decoder attention mask automatically.
+            "labels": labels.flatten(),
         }
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Test loading natural instructions dataset.")
+    parser.add_argument("--config", type=str, default="./dev_config.json", help="Path to config file")
+    args = parser.parse_args()
+
+    # Load the config
+    with open(args.config, "r") as f:
+        config = parsed = DotMap(json.load(f))
+
+    tokenizer = BartTokenizerFast.from_pretrained("facebook/bart-base")
+    print(f"Finished initializing tokenizer")
+
+    val_dataset = NaturalInstructionsV1Seq2SeqDataset(
+        subtask_dir=config.subtask_dir,
+        tasks=config.val_tasks,
+        template=config.template,
+        pos_template=config.pos_template,
+        neg_template=config.neg_template,
+        tokenizer=tokenizer,
+        max_input_length=config.max_input_length,
+        max_output_length=config.max_output_length,
+    )
+    print(f"Finished loading datasets")
+
+    for i, batch in enumerate(val_dataset):
+        print("Sample Index:", i)
+        input_str = tokenizer.decode(batch["input_ids"])
+        output_ids = [token_id if token_id != HF_LOSS_IGNORE_TOKEN_ID else tokenizer.pad_token_id for token_id in batch["labels"]]
+        print("Input:\n", tokenizer.decode(batch["input_ids"]))
+        print("Output:\n", tokenizer.decode(output_ids))
+        num_non_padded_tokens = len([token_id for token_id in batch["input_ids"] if token_id != tokenizer.pad_token_id])
+        print("Number of non-padded tokens in inputs:", num_non_padded_tokens)
+        num_non_padded_tokens = len([token_id for token_id in output_ids if token_id != tokenizer.pad_token_id])
+        print("Number of non-padded tokens in outputs:", num_non_padded_tokens)
+
+        if i > 1000:
+            break
--- train.py
+++ train.py
@@ -1,147 +1,146 @@
 import argparse
-import os
 import time
 import random
+import json
+
 import numpy as np
 import torch
 import torch.optim as optim
+from dotmap import DotMap
 from torch.utils.data import DataLoader
 from torch import autocast
 from torch.cuda.amp import GradScaler
 from torch.utils.tensorboard import SummaryWriter
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from transformers import T5TokenizerFast, T5ForConditionalGeneration
 from cosine_annealing_warmup import CosineAnnealingWarmupRestarts
-from data import HFCLMDataset
+from data import NaturalInstructionsV1Seq2SeqDataset
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Train GPT-2 on WikiText-2.")
-    parser.add_argument("--model-type", type=str, default="gpt2-large", help="Type of the model to train.")
-    parser.add_argument("--model-name", type=str, default="gpt2-large-finetuned", help="Name of the model to train.")
-    parser.add_argument("--load-model", type=str, default=None, help="Path to load model from.")
-
-    parser.add_argument("--max_length", type=int, default=1024, help="Maximum length of the input sequence.")
-    parser.add_argument("--batch-size", type=int, default=32, help="Batch size for training.")
-    parser.add_argument("--eval-batch-size", type=int, default=8, help="Batch size for evaluation.")
-    parser.add_argument("--gradient-accumulation-steps", type=int, default=32, help="Number of steps for gradient accumulation.")
-    parser.add_argument("--weight-decay", type=float, default=0.01, help="Weight decay for the optimizer.")
-    parser.add_argument("--max-lr", type=float, default=1e-5)
-    parser.add_argument("--min-lr", type=float, default=1e-5 * 0.01)
-    parser.add_argument("--cycle-steps", type=int, default=50_000)
-    parser.add_argument("--warmup-steps", type=int, default=5_000)
-    parser.add_argument("--lr-gamma", type=float, default=0.5)
-
-    parser.add_argument("--num-epochs", type=int, default=10, help="Number of epochs for training.")
-    parser.add_argument("--eval-very-n-steps", type=int, default=1_000, help="Evaluate model every X batches.")
-
-    parser.add_argument("--fp16", action="store_true", help="Use mixed precision training.")
-    parser.add_argument("--compile", action="store_true", help="Compile the model for faster training.")
-    parser.add_argument("--activation-checkpointing", action="store_true", help="Use activation checkpointing to save memory")
-
-    parser.add_argument("--seed", type=int, default=456970, help="Random seed for reproducibility.")
-    parser.add_argument("--device", type=str, default="cuda", help="Device to use for training. (cuda, cpu)")
-    parser.add_argument("--num-workers", type=int, default=2, help="Number of workers for the dataloader.")
-
+    parser = argparse.ArgumentParser(description="Finetune Flan-T5 on natural instructions dataset.")
+    parser.add_argument("--config", type=str, default="./dev_config.json", help="Path to config file")
     args = parser.parse_args()
 
-    # Set up the environment
-    log_writer = SummaryWriter(f"./logs/{args.model_name}")
+    # Load the config
+    with open(args.config, "r") as f:
+        config = parsed = DotMap(json.load(f))
 
-    torch.manual_seed(args.seed)
-    np.random.seed(args.seed)
-    random.seed(args.seed)
-    device = torch.device("cuda" if torch.cuda.is_available() and args.device == "cuda" else "cpu")
+    # Set up the environment
+    log_writer = SummaryWriter(f"./logs/{config.model_name}")
+
+    torch.manual_seed(config.seed)
+    np.random.seed(config.seed)
+    random.seed(config.seed)
+    device = torch.device("cuda" if torch.cuda.is_available() and config.device == "cuda" else "cpu")
     torch.backends.cudnn.benchmark = True
     torch.backends.cuda.matmul.allow_tf32 = True
     torch.backends.cudnn.allow_tf32 = True
     print(f"Finished setting up device: {device}")
 
-    # 1. Initialize the tokenizer
-    tokenizer = GPT2Tokenizer.from_pretrained(args.model_type)
+    # Initialize the tokenizer
+    tokenizer = T5TokenizerFast.from_pretrained(config.model_type)
     print(f"Finished initializing tokenizer")
 
     # Initialize the datasets
-    train_dataset = HFCLMDataset("wikitext", "wikitext-2-raw-v1", tokenizer, dataset_type="train", max_length=args.max_length)
-    valid_dataset = HFCLMDataset("wikitext", "wikitext-2-raw-v1", tokenizer, dataset_type="validation", max_length=args.max_length)
+    train_dataset = NaturalInstructionsV1Seq2SeqDataset(
+        subtask_dir=config.subtask_dir,
+        tasks=config.train_tasks,
+        template=config.template,
+        pos_template=config.pos_template,
+        neg_template=config.neg_template,
+        tokenizer=tokenizer,
+        max_input_length=config.max_input_length,
+        max_output_length=config.max_output_length,
+    )
+
+    val_dataset = NaturalInstructionsV1Seq2SeqDataset(
+        subtask_dir=config.subtask_dir,
+        tasks=config.val_tasks,
+        template=config.template,
+        pos_template=config.pos_template,
+        neg_template=config.neg_template,
+        tokenizer=tokenizer,
+        max_input_length=config.max_input_length,
+        max_output_length=config.max_output_length,
+    )
     print(f"Finished loading datasets")
 
     # Initialize the dataloaders
     train_dataloader = DataLoader(
         train_dataset,
-        batch_size=args.batch_size // args.gradient_accumulation_steps,
+        batch_size=config.batch_size // config.gradient_accumulation_steps,
         shuffle=True,
         drop_last=True,
-        num_workers=args.num_workers,
+        num_workers=config.num_workers,
     )
-    valid_dataloader = DataLoader(valid_dataset, batch_size=args.eval_batch_size, shuffle=False, drop_last=True, num_workers=args.num_workers)
+    valid_dataloader = DataLoader(val_dataset, batch_size=config.eval_batch_size, shuffle=False, drop_last=True, num_workers=config.num_workers)
 
     # Initialize the model and optimizer
-    model = GPT2LMHeadModel.from_pretrained(args.model_type).to(device)
-    if args.load_model is not None:
-        model.load_state_dict(torch.load(args.load_model, map_location=device))
-    if args.activation_checkpointing:
+    model = T5ForConditionalGeneration.from_pretrained(config.model_type).to(device)
+    if config.load_model is not None:
+        model.load_state_dict(torch.load(config.load_model, map_location=device))
+    if config.activation_checkpointing:
         model.gradient_checkpointing_enable()
-    model = torch.compile(model, disable=not args.compile)
+    model = torch.compile(model, disable=not config.compile)
     print(f"Finished loading model")
 
-    optimizer = optim.AdamW(model.parameters(), lr=args.max_lr, weight_decay=args.weight_decay)
+    optimizer = optim.AdamW(model.parameters(), lr=config.max_lr, weight_decay=config.weight_decay)
     scheduler = CosineAnnealingWarmupRestarts(
         optimizer,
-        first_cycle_steps=args.cycle_steps,
+        first_cycle_steps=config.cycle_steps,
         cycle_mult=1.0,
-        max_lr=args.max_lr,
-        min_lr=args.min_lr,
-        warmup_steps=args.warmup_steps,
-        gamma=args.lr_gamma,
+        max_lr=config.max_lr,
+        min_lr=config.min_lr,
+        warmup_steps=config.warmup_steps,
+        gamma=config.lr_gamma,
     )
     print("Initialized optimizer.")
-    print(f"Finished initializing model and optimizer")
 
     best_eval_loss = 10.0**9
 
     model.train()
-    scaler = GradScaler(enabled=args.fp16)
+    scaler = GradScaler(enabled=config.fp16)
 
     batch_loss = 0.0
     step_count = 0
     batch_count = 0
     last_batch_time = time.time()
 
-    for epoch in range(args.num_epochs):
+    for epoch in range(config.num_epochs):
         for batch in train_dataloader:
-            with autocast(device_type=str(device), dtype=torch.float16, enabled=args.fp16):
+            with autocast(device_type=str(device), dtype=torch.float16, enabled=config.fp16):
                 inputs = batch["input_ids"].to(device)
                 labels = batch["labels"].to(device)
                 mask = batch["attention_mask"].to(device)
 
                 outputs = model(input_ids=inputs, attention_mask=mask, labels=labels)
                 loss = outputs.loss
-                loss /= args.gradient_accumulation_steps
+                loss /= config.gradient_accumulation_steps
 
             scaler.scale(loss).backward()
             step_count += 1
             batch_loss += float(loss.item())
 
-            if step_count % args.gradient_accumulation_steps == 0:
+            if step_count % config.gradient_accumulation_steps == 0:
                 scaler.step(optimizer)
                 scaler.update()
                 optimizer.zero_grad()
                 scheduler.step()
                 batch_count += 1
                 print(
-                    f"Epoch {epoch}, Batch {batch_count}, Loss {batch_loss}, samples/sec {args.batch_size / (time.time() - last_batch_time)}"
+                    f"Epoch {epoch}, Batch {batch_count}, Loss {batch_loss}, samples/sec {config.batch_size / (time.time() - last_batch_time)}"
                 )
                 log_writer.add_scalar("train_loss", batch_loss, batch_count)
                 batch_loss = 0.0
                 last_batch_time = time.time()
 
-                if batch_count > 0 and batch_count % args.eval_batch_count == 0:
+                if batch_count > 0 and batch_count % config.eval_very_n_steps == 0:
                     model.eval()
                     eval_loss = 0
                     eval_start_time = time.time()
                     with torch.no_grad():
                         for batch in valid_dataloader:
-                            with autocast(device_type=str(device), dtype=torch.float16, enabled=args.fp16):
+                            with autocast(device_type=str(device), dtype=torch.float16, enabled=config.fp16):
                                 inputs = batch["input_ids"].to(device)
                                 labels = batch["labels"].to(device)
                                 mask = batch["attention_mask"].to(device)
@@ -152,15 +151,15 @@ if __name__ == "__main__":
                     eval_loss /= len(valid_dataloader)
                     log_writer.add_scalar("eval_loss", eval_loss, batch_count)
                     print(f"Batch count {batch_count}, Eval loss {eval_loss}, " + 
-                          f"samples/sec {len(valid_dataloader) * args.eval_batch_size / (time.time() - eval_start_time)}")
+                          f"samples/sec {len(valid_dataloader) * config.eval_batch_size / (time.time() - eval_start_time)}")
 
                     if eval_loss < best_eval_loss:
                         best_eval_loss = eval_loss
-                        torch.save(model.state_dict(), f"./{args.model_name}_best.pt")
+                        torch.save(model.state_dict(), f"./{config.model_name}_best.pt")
 
-                    torch.save(model.state_dict(), f"./{args.model_name}_latest.pt")
+                    torch.save(model.state_dict(), f"./{config.model_name}_latest.pt")
                     print(f"Saving latest model with loss {eval_loss}, batch {batch_count}")
 
                     model.train()
 
-    print(f"Finished training for {args.num_epochs} epochs, {batch_count} batches with best eval loss {best_eval_loss}")
+    print(f"Finished training for {config.num_epochs} epochs, {batch_count} batches with best eval loss {best_eval_loss}")
